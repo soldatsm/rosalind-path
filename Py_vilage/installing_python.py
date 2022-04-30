3cb57dcde513c387ec0a512fd477335f97bbb138
soldatsm <soldatsm@yandex.ru>
2022-04-30 11:21:29 +0300

    some tests

---
+++ Py_vilage/installing_python.py
@@ -0,0 +1,17 @@
+import this
+
+def solution():
+    """
+    Just very specificly write out Zqn op Python
+    """
+    with open('solution_ini1.txt', 'a') as write_file:
+        write_file.writelines(''.join(this.d.get(el, el) for el in this.s))
+
+if __name__ == '__main__':
+    solution()
+
+
+# Actulally module `this` much more interesting
+# then you can think
+# https://stackoverflow.com/questions/43577404/purpose-of-import-this
+# https://stackoverflow.com/questions/37301273/attributes-of-python-module-this
